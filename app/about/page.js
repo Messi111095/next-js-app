de75de280e7b8903d0469e98826b66f9cc8102e6
Messi111095 <ashutoshkumar111095@gmail.com>
2026-07-02 10:44:56 +0530

    intro next.js course

---
+++ app/about/page.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>About Us</h1>
+      <p>Welcome to our website! We are dedicated to providing the best service possible.</p>
+    </main>
+  );
+}
