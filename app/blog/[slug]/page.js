4f7109d788bdc83b7efeaa079328a8b4b0b00a62
Messi111095 <ashutoshkumar111095@gmail.com>
2026-07-02 15:23:43 +0530

    demo next.js date 02.07.2026

---
+++ app/blog/[slug]/page.js
@@ -0,0 +1,12 @@
+
+
+export default function BlogPostPage({params}) {
+//   const { slug } = params;
+
+  return (
+    <main>
+      <h1>Blog Post</h1>
+      {/* <p>This is the content for the blog post with slug: {slug}</p> */}
+    </main>
+  );
+}
